74f1bbcb98b07c5c038a1cc7b2d42927d9d78cfe
Ahnaf Sakil <55395335+Ahnaf16@users.noreply.github.com>
2025-05-02 01:28:49 +0600

    inventory_record ui

--- lib/_core/strings/app_strings.dart
+++ lib/_core/strings/app_strings.dart
@@ -3,4 +3,6 @@ class Str {
   static const String data = 'data';
  
   static const String staffNotFound = 'Staff not found';
+ 
+  static const String calculation = 'calculation';
  }
--- lib/features/inventory_record/view/create_record_view.dart
+++ lib/features/inventory_record/view/create_record_view.dart
@@ -24,16 +24,161 @@ class CreateRecordView extends HookConsumerWidget {
           },
         ),
       ],
-      body: SingleChildScrollView(
-        child: FormBuilder(
-          key: formKey,
-          child: const Column(
-            spacing: Insets.lg,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [Gap(Insets.xl)],
+      body: Row(
+        spacing: Insets.sm,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Column(
+              spacing: Insets.sm,
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+                Row(
+                  children: [
+                    ShadSelect<int>.withSearch(
+                      itemCount: 20,
+                      maxWidth: 300,
+                      minWidth: 300,
+                      placeholder: const Text('parti'),
+                      optionsBuilder: (_, i) => ShadOption<int>(value: i, child: Text('$i')),
+                      selectedOptionBuilder: (_, v) => Text(v.toString()),
+                      onSearchChanged: (value) {},
+                    ),
+                    ShadIconButton.outline(height: 38, icon: const Icon(LuIcons.plus), onPressed: () {}),
+                  ],
+                ),
+                Expanded(
+                  child: ShadCard(
+                    padding: Pads.zero,
+                    child: ShadResizablePanelGroup(
+                      axis: Axis.vertical,
+                      showHandle: true,
+                      children: [
+                        ShadResizablePanel(
+                          id: 0,
+                          defaultSize: 0.7,
+                          child: Padding(
+                            padding: Pads.sm('b'),
+                            child: ListView.separated(
+                              padding: Pads.sm(),
+                              itemCount: 30,
+                              separatorBuilder: (_, _) => const Gap(Insets.med),
+                              itemBuilder: (BuildContext context, int index) {
+                                return _box('$index', 50);
+                              },
+                            ),
+                          ),
+                        ),
+                        ShadResizablePanel(
+                          id: 1,
+                          defaultSize: 0.3,
+                          minSize: 0.1,
+                          child: SingleChildScrollView(
+                            padding: Pads.sm(),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              spacing: Insets.sm,
+                              children: [
+                                Expanded(
+                                  flex: 2,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    spacing: Insets.sm,
+                                    children: [
+                                      const Row(
+                                        children: [
+                                          Expanded(flex: 2, child: ShadField(hintText: 'Amount')),
+                                          Expanded(child: ShadField(hintText: 'Vat')),
+                                        ],
+                                      ),
+
+                                      const Row(
+                                        children: [
+                                          Expanded(child: ShadField(hintText: 'Discount')),
+                                          Expanded(child: ShadField(hintText: 'Shipping')),
+                                        ],
+                                      ),
+                                      ShadSelect<int>(
+                                        itemCount: 5,
+                                        maxWidth: 300,
+                                        minWidth: 300,
+                                        placeholder: const Text('account'),
+                                        optionsBuilder: (_, i) => ShadOption<int>(value: i, child: Text('$i')),
+                                        selectedOptionBuilder: (_, v) => Text(v.toString()),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: 200, child: ShadSeparator.vertical()),
+                                Expanded(
+                                  child: Column(
+                                    spacing: Insets.sm,
+                                    children: [
+                                      SpacedText(
+                                        left: 'Subtotal',
+                                        right: 10000.currency(),
+                                        styleBuilder: (l, r) => (l, r.bold),
+                                      ),
+                                      SpacedText(
+                                        left: 'Total',
+                                        right: 10000.currency(),
+                                        styleBuilder: (l, r) => (l, context.text.large),
+                                      ),
+                                      SpacedText(left: 'Due', right: 0.currency(), styleBuilder: (l, r) => (l, r.bold)),
+                                      const Gap(Insets.xs),
+                                      SubmitButton(
+                                        width: double.infinity,
+                                        onPressed: (l) {},
+                                        child: const Text('Submit'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Expanded(
+            child: Column(
+              spacing: Insets.med,
+              children: [
+                const ShadInput(placeholder: Text('Search')),
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: Insets.sm,
+                      crossAxisSpacing: Insets.sm,
+                    ),
+                    itemCount: 20,
+                    itemBuilder: (context, index) {
+                      return _box('${index + 1}');
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _box(String name, [double? size]) {
+    return DecoContainer(
+      size: size,
+      color: Colors.grey.shade800,
+      borderRadius: Corners.med,
+      alignment: Alignment.center,
+      child: Text(name),
     );
   }
 }
